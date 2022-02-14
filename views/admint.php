6f029fb405aff15125fc932f0c4af4078a84df21
Jhojann <jhostriana11@misena.edu.co>
2022-02-14 16:40:16 -0500

    admint

--- views/admint.php
+++ views/admint.php
@@ -69,6 +69,28 @@
         </div>
     </div>
 
+    <!--Modal de fecha-->
+    <div class="modal fade" id="fecha" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Agregar una fecha no disponible</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="">
+                        <label for="">Ingrese la fecha a cambiar de estado a "NO DISPONIBLE"</label>
+                        <input type="date" class="form-control">
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="content">
         <div class="section">
             <div class="d-flex">
@@ -184,6 +206,14 @@
                 Registrar usuario
             </button>
         </div>
+
+        <!--Fechas no disponibles-->
+        <div class="section">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#fecha">
+                Crear fechas
+            </button>
+        </div>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
