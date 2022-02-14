e9b363d4dd17e451c5e5ebf0ebd5c0e6c4a638d4
Jhojann <jhostriana11@misena.edu.co>
2022-02-14 10:17:10 -0500

    comentario de hola Diego

--- views/admint.php
+++ views/admint.php
@@ -17,12 +17,15 @@
 </head>
 
 <body>
+    <h1>Hola Diego</h1>
     <?php
     include "layouts/nav.php";
     ?>
 
-    <div class="content section">
-        <h1>Hola Mundo</h1>
+    <div class="content">
+        <div class="section">
+
+        </div>
     </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
