19dcdcecb1d376a9d66ffa2a9b96cc166a3493c3
Jhojann <jhostriana11@misena.edu.co>
2022-02-19 14:08:46 -0500

    TITLE

--- views/admint.php
+++ views/admint.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Administrador</title>
     <link rel="stylesheet" href="<?php echo constant('URL') ?>/public/css/styles.css">
     <style>
         body {
