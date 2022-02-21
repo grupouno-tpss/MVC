164ca2ad568d6e43a292214d291294eb3d0afc4e
Jhojann <jhostriana11@misena.edu.co>
2022-02-21 16:33:45 -0500

    --

--- libs/view.php
+++ libs/view.php
@@ -7,5 +7,8 @@
         public function render ($name, $value) {
             require 'views/'.$name.'.php';
         }
+
+        //$name = nombre del archivo de la carpeta de interfaces
+
     }
 ?>
